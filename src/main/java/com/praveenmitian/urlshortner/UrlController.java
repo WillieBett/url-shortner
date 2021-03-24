1fd3504b82301ea22ae37768bcffcf23d8262f76
Wilfred Bett <kimutaibett30@gmail.com>
2021-03-24 04:47:15 -0700

    Update UrlController.java

--- src/main/java/com/praveenmitian/urlshortner/UrlController.java
+++ src/main/java/com/praveenmitian/urlshortner/UrlController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Controller class process the request based on the query.
  * 
- * @author PRAVEEN
+ * @author Wilfred Bett
  *
  */
 @RestController
@@ -43,112 +43,9 @@ public class UrlController {
 		return new Url(encodedId, url.getLongUrl());
 	}
 
-	/**
-	 * Controller process the request and get method used to expand the short url.
-	 * 
-	 * @param urlId
-	 * @return String
-	 */
-	@RequestMapping(value = "/expand/{urlId}", method = RequestMethod.GET)
-	public String expandUrl(@PathVariable String urlId) {
-		int count = urlJDBC.count(urlId);
-		urlJDBC.update(urlId, count + 1);
-		List<Url> urlList = urlJDBC.getUrl(urlId);
-		if (urlList != null && urlList.size() > 0)
-			return urlList.get(0).getLongUrl();
-		else
-			return "Match not found for \'" + urlId + "\'";
-	}
+	
+	
 
-	/**
-	 * Controller process the request and get method display top 5 most viewed
-	 * shorten urls.
-	 * 
-	 * @return List<Url>
-	 */
-	@RequestMapping(value = "/mostpopular", method = RequestMethod.GET)
-	public List<Url> mostViewedUrls() {
-		return urlJDBC.mostViewedUrls();
-	}
-
-	/**
-	 * Controller process the request and get method display top 5 recently
-	 * viewed shorten urls.
-	 * 
-	 * @return List<Map<String, Object>>
-	 */
-	@RequestMapping(value = "/recentlyviewed", method = RequestMethod.GET)
-	public List<Map<String, Object>> latest() {
-		return urlJDBC.recentlyViewedUrls();
-	}
-
-	/**
-	 * Controller process the request and get method display list of shorten
-	 * urls till date.
-	 * 
-	 * @return List<String>
-	 */
-	@RequestMapping(value = "/shorturllist", method = RequestMethod.GET)
-	public List<String> all() {
-		return urlJDBC.listOfShortenUrls();
-	}
-
-	/**
-	 * Controller process the request and get method display total count of Url
-	 * shorten till date.
-	 * 
-	 * @return int
-	 */
-	@RequestMapping(value = "/urlstotalcount", method = RequestMethod.GET)
-	public int totalUrlsShortened() {
-		return urlJDBC.totalUrlsShortened();
-	}
-
-	/**
-	 * Controller process the request and get method display top 5 recently
-	 * shorten urls.
-	 * 
-	 * @return List<Map<String, Object>>
-	 */
-	@RequestMapping(value = "/recentlyshorten", method = RequestMethod.GET)
-	public List<Map<String, Object>> recentlyShortenUrls() {
-		return urlJDBC.recentlyShortenUrls();
-	}
-
-	/**
-	 * Controller process the request and get method returns each day count of
-	 * shorten url for past 5 days.
-	 * 
-	 * @return
-	 */
-	@RequestMapping(value = "/last5dayscount", method = RequestMethod.GET)
-	public List<Map<String, Object>> chart() {
-		List<Map<String, Object>> lm = urlJDBC.last5dayscount();
-		Calendar c = null;
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
-		Map<String, Object> dayCountMap = null;
-		String date = null;
-		for (int i = 0; i < 5; i++) {
-			c = Calendar.getInstance();
-			c.add(Calendar.DAY_OF_MONTH, -i);
-			date = sdf.format(c.getTime());
-			dayCountMap = new HashMap<String, Object>();
-			dayCountMap.put("DATE", c.getTime().getTime());
-			dayCountMap.put("COUNT", 0);
-			result.add(dayCountMap);
-			for (Map<String, Object> map : lm) {
-				if (((java.sql.Date) map.get("DATE")).toString().equals(date)) {
-					dayCountMap.put("COUNT", map.get("COUNT"));
-					break;
-				}
-			}
-		}
-		return result;
-	}
-
-	@RequestMapping(value = "/listurl", method = RequestMethod.GET)
-	public List<Map<String, Object>> urlList() {
-		return urlJDBC.urlList();
-	}
+	
+	
 }
